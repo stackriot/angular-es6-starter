ff95b7698a00894ad6454abdfe95862e12bda83f
codekraft-studio <info@codekraft.it>
2016-06-11 00:34:58 +0200

    some changes + routes list

--- src/app/controllers/app.controller.js
+++ src/app/controllers/app.controller.js
@@ -1,10 +1,19 @@
 class MainCtrl {
 
-  constructor(MainService) {
-    this.title = 'Angular ES6';
-    this.description = 'start here your Angular ES6 project';
-  }
+    constructor($route) {
+        this.title = 'Angular ES6';
+        this.description = 'start here your Angular ES6 project';
+        this.routes = getRoutes($route);
+    }
 
+}
+
+function getRoutes($route) {
+    let arr = [];
+    angular.forEach($route.routes, function (route, val) {
+        if(route.title) { route.url = '#' + route.originalPath; arr.push(route); }
+    })
+    return arr;
 }
 
 export default MainCtrl;
