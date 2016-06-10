4b6e9d5e6d5b095c7cbf89d061de1e0f03c90a4e
codekraft-studio <info@codekraft.it>
2016-06-11 00:34:42 +0200

    some changes

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,18 @@
+import '../../index.html';
+import '../../style.css';
+
 import angular from 'angular';
+import angularRoute from 'angular-route';
+import AppConfig from './app.config';
+
+import HomeModule from './components/home/home.module';
 import MainCtrl from './controllers/app.controller';
 import MainService from './services/app.service';
 import MainDirective from './directives/app.directive';
 
-angular.module('app', [])
+let app = angular.module('app', [angularRoute, HomeModule.name])
 
+  .config(AppConfig)
   .controller('MainCtrl', MainCtrl)
   .service('MainService', MainService)
   .directive('mainDirective', MainDirective)
